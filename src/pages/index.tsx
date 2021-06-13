f50f17ec962eccbc28ad739402ed301042c53ac3
unknown <otavio.paiva93@yahoo.com.br>
2021-06-13 14:24:16 -0300

    Front ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,88 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <main className={styles.mainContainer}>
+        <Header />
+        <div className={styles.postBoard}>
+          <h1>Como utilizar Hooks</h1>
+          <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+          <div className={styles.postInfo}>
+            <div className={styles.postData}>
+              <FiUser />
+              <p>15 Mar 2021</p>
+            </div>
+            <div className={styles.postAutor}>
+              <FiUser />
+              <p>Joseph Oliveira</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.postBoard}>
+          <h1>Como utilizar Hooks</h1>
+          <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+          <div className={styles.postInfo}>
+            <div className={styles.postData}>
+              <FiUser />
+              <p>15 Mar 2021</p>
+            </div>
+            <div className={styles.postAutor}>
+              <FiUser />
+              <p>Joseph Oliveira</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.postBoard}>
+          <h1>Como utilizar Hooks</h1>
+          <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+          <div className={styles.postInfo}>
+            <div className={styles.postData}>
+              <FiUser />
+              <p>15 Mar 2021</p>
+            </div>
+            <div className={styles.postAutor}>
+              <FiUser />
+              <p>Joseph Oliveira</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.postBoard}>
+          <h1>Como utilizar Hooks</h1>
+          <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+          <div className={styles.postInfo}>
+            <div className={styles.postData}>
+              <FiUser />
+              <p>15 Mar 2021</p>
+            </div>
+            <div className={styles.postAutor}>
+              <FiUser />
+              <p>Joseph Oliveira</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.postBoard}>
+          <h1>Como utilizar Hooks</h1>
+          <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+          <div className={styles.postInfo}>
+            <div className={styles.postData}>
+              <FiUser />
+              <p>15 Mar 2021</p>
+            </div>
+            <div className={styles.postAutor}>
+              <FiUser />
+              <p>Joseph Oliveira</p>
+            </div>
+          </div>
+        </div>
+        <button className={styles.loadPosts} type="button">
+          Carregar mais posts
+        </button>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
